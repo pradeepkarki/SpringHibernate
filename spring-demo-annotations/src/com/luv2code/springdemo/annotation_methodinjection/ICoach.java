6fda4047f799b2d01f6b0ccadd32e0c8ca592575
Administrator <Administrator@R0349>
2018-05-24 17:05:06 +0530

    New Package

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/annotation_methodinjection/ICoach.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo.annotation_methodinjection;
+
+public interface ICoach {
+
+	public String getDailyWorkout();
+
+	public String getDailyFortune();
+
+}
---
+++ spring-demo-annotations/src/com/luv2code/springdemo/annotation_methodinjection/IFortuneService.java
@@ -0,0 +1,7 @@
+package com.luv2code.springdemo.annotation_methodinjection;
+
+public interface IFortuneService {
+
+	public String getFortune();
+
+}

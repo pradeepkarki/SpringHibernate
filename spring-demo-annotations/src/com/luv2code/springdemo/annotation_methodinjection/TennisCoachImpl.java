b3753b21d3d83b8942d388ac351d868996df1b82
darkprince <darkprince@darkprince-PC>
2018-05-25 14:33:41 +0530

    annotation method injection

--- spring-demo-annotations/src/com/luv2code/springdemo/annotation_methodinjection/TennisCoachImpl.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/annotation_methodinjection/TennisCoachImpl.java
@@ -1,25 +1,31 @@
 package com.luv2code.springdemo.annotation_methodinjection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import com.luv2code.springdemo.annotation_constructorInjection.IFortuneService;
 
 //@Component("myTennisCoach")
 
 @Component
 public class TennisCoachImpl implements ICoach {
 
+	//Field injection
+	@Autowired
 	private IFortuneService fortuneService;
 
 	// Default constructor
 	public TennisCoachImpl() {
 		System.out.println(">> inside default constructor");
 	}
-
-	@Autowired
+	
+	/*//@Autowired
+	 *method injection
 	public void methodInjection(IFortuneService fortuneService1) {
 		System.out.println("TennisCoachImpl: inside methodInjection() method ");
 		fortuneService = fortuneService1;
-	}
+	}*/
 
 	@Override
 	public String getDailyWorkout() {
